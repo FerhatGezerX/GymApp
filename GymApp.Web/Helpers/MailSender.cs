194038b5354dbb26d1ec4a83e440344d855077b9
FerhatGezerX <ferhat.gzr03@gmail.com>
2024-05-12 21:06:02 +0300

    İnital comment

---
+++ GymApp.Web/Helpers/MailSender.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace GymApp.Web.Helpers
+{
+    public static class MailSender
+    {
+        private const string SenderMail = "ferhat.gzr96@outlook.com";  //const = sabit değer için kullanılıyor.
+        private const string SenderPassword = "Ferhat1296";
+
+        public static void SendMail(string fullName, List<string> mailAddresses)
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress(SenderMail);
+
+                for (int i = 0; i < mailAddresses.Count; i++)
+                {
+                    mailMessage.To.Add(mailAddresses[i]);
+                }
+                mailMessage.Subject = "Hoş Geldiniz";
+                mailMessage.Body = $"Merhaba {fullName}, \nSitemize hoş geldiniz.";
+                mailMessage.IsBodyHtml = true;
+
+
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Host = "smtp-mail.outlook.com";
+                smtpClient.Port = 587;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(SenderMail, SenderPassword);
+                smtpClient.EnableSsl = true;
+
+                smtpClient.Send(mailMessage);
+                Console.WriteLine("Email Sent Successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ GymApp.Web/Migrations/20240508182151_ContactMessage.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymApp.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContactMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutRedirectUrl",
+                table: "MainPages",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutImageUrl3",
+                table: "MainPages",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutImageUrl2",
+                table: "MainPages",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutImageUrl1",
+                table: "MainPages",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutRedirectUrl",
+                table: "MainPages",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutImageUrl3",
+                table: "MainPages",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutImageUrl2",
+                table: "MainPages",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutImageUrl1",
+                table: "MainPages",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+        }
+    }
+}
